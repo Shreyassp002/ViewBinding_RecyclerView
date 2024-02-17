10b793da3fec1de80ca9a6aaa829e15bceb06e59
Shreyas Patil <shreyassp002@gmail.com>
2024-02-18 00:27:32 +0530

    Initial Commit

---
+++ app/src/main/java/com/rey/viewbinding_recyclerview/Task.kt
@@ -0,0 +1,3 @@
+package com.rey.viewbinding_recyclerview
+
+data class Task(val title: String, val timeStamp: String)
---
+++ app/src/main/java/com/rey/viewbinding_recyclerview/TaskList.kt
@@ -0,0 +1,27 @@
+package com.rey.viewbinding_recyclerview
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Wake Up", "7:00 AM"),
+        Task("Breakfast", "7:30 AM"),
+        Task("Attend Meeting", "8:00 AM"),
+        Task("Reed Book", "12:00 PM"),
+        Task("Lunch", "1:00 PM"),
+        Task("Take a Nap", "2:00 PM"),
+        Task("Play Cricket", "4:00 PM"),
+        Task("Study Kotlin and Make Apps", "6:00 PM"),
+        Task("Dinner", "8:00 PM"),
+        Task("Sleep", "10:00 PM"),
+        Task("Wake Up", "7:00 AM"),
+        Task("Breakfast", "7:30 AM"),
+        Task("Attend Meeting", "8:00 AM"),
+        Task("Reed Book", "12:00 PM"),
+        Task("Lunch", "1:00 PM"),
+        Task("Take a Nap", "2:00 PM"),
+        Task("Play Cricket", "4:00 PM"),
+        Task("Study Kotlin and Make Apps", "6:00 PM"),
+        Task("Dinner", "8:00 PM"),
+        Task("Sleep", "10:00 PM")
+
+    )
+}
